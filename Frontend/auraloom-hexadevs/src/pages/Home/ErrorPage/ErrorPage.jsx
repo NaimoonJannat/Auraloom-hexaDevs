1021009be62bd5039f6e2ac0c0467737dccc5062
Idba1 <moniraislamself@gmail.com>
2024-09-19 02:33:35 +0600

    add error page

---
+++ Frontend/auraloom-hexadevs/src/pages/Home/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='text-3xl text-red-700 text-center'>
+            <h1>404</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
