16a03490b2df9406cfd37642cbc9d1397cad004f
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-09-26 12:26:24 +0600

    trending and top content section added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import TopContent from "@/components/TopContent";
+import TrendingContent from "@/components/TrendingContent";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +25,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TrendingContent />
+        <TopContent />
         {children}
       </body>
     </html>
