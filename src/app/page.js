3e5a9f9bc13b719112433cae536b92f54f0bde40
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-09-26 20:32:02 +0600

    navbar updated new tech(next.js)

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+import Navbar from "@/Components/Navbar/Navbar";
+import TopContent from "@/Components/TopContent";
+import TrendingContent from "@/Components/TrendingContent";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="container mx-auto">
+      <Navbar></Navbar>
+      <TrendingContent />
+      <TopContent />
     </div>
   );
 }
