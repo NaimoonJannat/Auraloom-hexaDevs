1e195bd63d11bb058198ce3354e6c545ea545440
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-09-27 01:42:37 +0600

    footer added

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,18 @@
+import Footer from "@/Components/Footer/Footer";
+import TopContent from "@/Components/Home/TopContent";
+import TrendingContent from "@/Components/Home/TrendingContent";
 import Navbar from "@/Components/Navbar/Navbar";
-import TopContent from "@/Components/TopContent";
-import TrendingContent from "@/Components/TrendingContent";
 import Link from "next/link";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <div className="container mx-auto">
-      <Navbar></Navbar>
-      <TrendingContent />
-      <TopContent />
-=======
     <div>
-      <h2 className="text-3xl text-center">Eitai home page -.- app thik moto run kortese</h2>
->>>>>>> 05ef0d76dd2408a46ec14f2a804a182c1a622381
+      <div className="container mx-auto">
+        <Navbar></Navbar>
+        <TrendingContent></TrendingContent>
+        <TopContent></TopContent>
+      </div>
+        <Footer></Footer>
     </div>
   );
 }
