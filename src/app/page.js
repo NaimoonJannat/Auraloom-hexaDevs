b8be81edeae6220ab7777b562f1cae11402d3c6d
NaimoonJannat <naimoonjannat@gmail.com>
2024-09-27 10:18:11 +0600

    Trending added homepage

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import CategorySelector from "@/Components/category-selector/CategorySelector";
 import Newsletter from "@/Components/Home/Newsletter";
 import TopCreator from "@/Components/Home/TopCreator";
 import TrendingContent from "@/Components/Home/TrendingContent";
+import TrendingTopic from "@/Components/Home/TrendingTopic";
 
 
 export default function Home() {
   return (
     <div className="container mx-auto">
+      <TrendingTopic></TrendingTopic>
       <TopCreator></TopCreator>
             <TrendingContent></TrendingContent>
             <CategorySelector></CategorySelector>
