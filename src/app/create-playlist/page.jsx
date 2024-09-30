990743a1676d413d029ad043b82e55f03ad5ae77
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-09-30 21:38:29 +0600

    modal added

--- src/app/create-playlist/page.jsx
+++ src/app/create-playlist/page.jsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image';
 import img1 from '../../../public/pexels-dmitry-demidov-515774-3783471.jpg'
 import { IoIosAddCircleOutline } from "react-icons/io";
@@ -14,7 +15,7 @@ const createPlaylist = () => {
 
 
             <div className=" mx-auto dark:text-gray-800">
-                <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded scale-75">
+                <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded scale-90">
 
                     <Image src={img1} alt="" className="w-full h-60 sm:h-96" />
                     <div className="p-6 pb- m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-50">
@@ -26,7 +27,7 @@ const createPlaylist = () => {
                         </div>
 
                         <div className="flex justify-center scale-90">
-                            <button type="button" className="px-8 py-3 font-semibold rounded-full bg-slate-200 hover:bg-[#90e0ef] flex items-center gap-2 text-lg">Add here <IoIosAddCircleOutline className='text-xl text-[#0077b6] font-bold' /> </button>
+                            <button type="button" onClick={() => document.getElementById('my_modal_1').showModal()} className="px-8 py-3 font-semibold rounded-full bg-slate-200 hover:bg-[#90e0ef] flex items-center gap-2 text-lg">Add here <IoIosAddCircleOutline className='text-xl text-[#0077b6] font-bold' /> </button>
                         </div>
                     </div>
                 </div>
@@ -34,20 +35,95 @@ const createPlaylist = () => {
 
 
             {/* MODAL */}
-            <div className="flex flex-col max-w-md gap-2 p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
-                <h2 className="flex items-center gap-2 text-xl font-semibold leading-tight tracking-wide">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-6 h-6 fill-current shrink-0 dark:text-violet-600">
-                        <path d="M451.671,348.569,408,267.945V184c0-83.813-68.187-152-152-152S104,100.187,104,184v83.945L60.329,348.568A24,24,0,0,0,81.432,384h86.944c-.241,2.636-.376,5.3-.376,8a88,88,0,0,0,176,0c0-2.7-.135-5.364-.376-8h86.944a24,24,0,0,0,21.1-35.431ZM312,392a56,56,0,1,1-111.418-8H311.418A55.85,55.85,0,0,1,312,392ZM94.863,352,136,276.055V184a120,120,0,0,1,240,0v92.055L417.137,352Z"></path>
-                        <rect width="32" height="136" x="240" y="112"></rect>
-                        <rect width="32" height="32" x="240" y="280"></rect>
-                    </svg>Necessitatibus dolores quasi quae?
-                </h2>
-                <p className="flex-1 dark:text-gray-600">Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
-                <div className="flex flex-col justify-end gap-3 mt-6 sm:flex-row">
-                    <button className="px-6 py-2 rounded-sm">No</button>
-                    <button className="px-6 py-2 rounded-sm shadow-sm dark:bg-violet-600 dark:text-gray-50">Yes</button>
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            {/* <button className="btn" onClick={() => document.getElementById('my_modal_1').showModal()}>open modal</button> */}
+
+            <dialog id="my_modal_1" className="modal">
+                <div className="modal-box max-w-4xl">
+                    {/* CARD */}
+                    <div className="bg-white ">
+                        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+
+                            <div className="">
+
+                                <div className="flex flex-col items-center gap-4 md:flex-row lg:gap-6 scale-90">
+                                    <a href="#" className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
+                                        <Image
+                                            src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600"
+                                            loading="lazy"
+                                            alt="Image description"
+                                            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                                            width={600}  // specify the width based on the image's size
+                                            height={400} // specify the height based on the image's aspect ratio
+                                        />
+                                    </a>
+
+
+                                    <div className="flex flex-col gap-2">
+                                        <span className="text-sm text-gray-400">July 19, 2021</span>
+
+                                        <h2 className="text-xl font-bold text-gray-800">
+                                            <a href="#" className="transition duration-100 hover:text-[#00b4d8] active:text-[#0077b6]">New trends in Tech</a>
+                                        </h2>
+
+                                        <p className="">This is a section of some simple filler text... </p>
+
+                                        <div>
+                                            <a href="#" className="font-semibold text-[#00b4d8] transition duration-100 hover:text-[#0077b6] active:text-indigo-700">Read more</a>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <button className='lg:pl-10'><IoIosAddCircleOutline className='text-3xl font-bold hover:text-[#0077b6]' /></button>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col items-center gap-4 md:flex-row lg:gap-6 scale-90">
+                                    <a href="#" className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
+                                        <Image
+                                            src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600"
+                                            loading="lazy"
+                                            alt="Image description"
+                                            className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                                            width={600}  // specify the width based on the image's size
+                                            height={400} // specify the height based on the image's aspect ratio
+                                        />
+                                    </a>
+
+
+                                    <div className="flex flex-col gap-2">
+                                        <span className="text-sm text-gray-400">July 19, 2021</span>
+
+                                        <h2 className="text-xl font-bold text-gray-800">
+                                            <a href="#" className="transition duration-100 hover:text-[#00b4d8] active:text-[#0077b6]">New trends in Tech</a>
+                                        </h2>
+
+                                        <p className="">This is a section of some simple filler text... </p>
+
+                                        <div>
+                                            <a href="#" className="font-semibold text-[#00b4d8] transition duration-100 hover:text-[#0077b6] active:text-indigo-700">Read more</a>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <button className='lg:pl-10'><IoIosAddCircleOutline className='text-3xl font-bold hover:text-[#0077b6]' /></button>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            </dialog>
 
         </div>
     );
