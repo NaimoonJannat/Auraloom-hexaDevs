e28eb3e2f17d27bec2b496f8bac331e21db4290b
RaisaNuzhat <raisa.csecu@gmail.com>
2024-10-02 18:26:28 +0600

    fixed ui of add podcast page

--- src/Components/add-podcast/AddPodcast.jsx
+++ src/Components/add-podcast/AddPodcast.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
-
+import { Typewriter } from 'react-simple-typewriter'
 const AddPodcast = () => {
   const [title, setTitle] = useState('');
   const [details, setDetails] = useState('');
@@ -69,98 +69,122 @@ const AddPodcast = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg"
-        encType="multipart/form-data"
-      >
-        <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Upload New Podcast</h2>
+    <div className='mx-auto my-20 container flex  flex-col justify-center items-center'>
+      <h1 className="mt-2 text-2xl lg:text-4xl font-bold text-[#03045e] capitalize  dark:text-white">
+        <Typewriter
+          words={['Bring Your Podcast to Life', 'Share Your Passion with the World!']}
+          loop={10}
+          cursor
+          cursorStyle='_'
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+      </h1>
+      <main className="relative z-20 w-full m-8 md:flex md:items-center xl:mt-12">
+        <div className="absolute inset-0 -z-10 md:h-[800px] rounded-2xl" style={{
+          backgroundImage: `url('/soundwave.svg')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}></div>
+        <div className="flex-grow h-full my-14 md:w-1/2 flex items-center justify-center p-3 translate-y-10">
+          <div className='flex flex-col  p-6 rounded-md sm:p-10 bg-[#90e0ef] bg-opacity-90 text-gray-900'>
+            <form
+              onSubmit={handleSubmit}
+              className="w-full max-w-md"
+              encType="multipart/form-data"
+            >
+              <h2 className="text-2xl font-bold mb-6 text-center text-[#03045e]">Upload New Podcast</h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {/* Title Field */}
+              <div className="mb-4 col-span-2">
+                <label className="block text-[#03045e] text-sm font-bold mb-2">
+                  Title of Podcast
+                </label>
+                <input
+                  type="text"
+                  name="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-[#0077b6] leading-tight focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  placeholder="Enter Podcast Title"
+                  required
+                />
+              </div>
 
-        {/* Title Field */}
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Title of Podcast
-          </label>
-          <input
-            type="text"
-            name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            placeholder="Enter Podcast Title"
-            required
-          />
-        </div>
+                {/* Details Field */}
+                <div className="mb-4 col-span-2">
+                <label className="block text-[#03045e] text-sm font-bold mb-2">
+                  Details (Mini blog or description)
+                </label>
+                <textarea
+                  name="details"
+                  value={details}
+                  onChange={(e) => setDetails(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-[#0077b6] leading-tight focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  placeholder="Enter podcast details"
+                  rows="4"
+                ></textarea>
+              </div>
 
-        {/* Wallpaper (Optional) */}
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Wallpaper (Optional)
-          </label>
-          <input
-            type="file"
-            name="wallpaper"
-            accept="image/*"
-            onChange={(e) => setWallpaper(e.target.files[0])}
-            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100"
-          />
-        </div>
+              {/* Wallpaper (Optional) */}
+              <div className="mb-4">
+                <label className="block text-[#03045e] text-sm font-bold mb-2">
+                  Wallpaper (Optional)
+                </label>
+                <input
+                  type="file"
+                  name="wallpaper"
+                  accept="image/*"
+                  onChange={(e) => setWallpaper(e.target.files[0])}
+                  className="block w-full text-sm text-[#0077b6] file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100"
+                />
+              </div>
 
-        {/* Audio Upload */}
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Attach the Audio
-          </label>
-          <input
-            type="file"
-            name="audio"
-            accept="audio/*"
-            onChange={(e) => setAudio(e.target.files[0])}
-            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100"
-          />
-        </div>
+              {/* Audio Upload */}
+              <div className="mb-4">
+                <label className="block text-[#03045e] text-sm font-bold mb-2">
+                  Attach the Audio
+                </label>
+                <input
+                  type="file"
+                  name="audio"
+                  accept="audio/*"
+                  onChange={(e) => setAudio(e.target.files[0])}
+                  className="block w-full text-sm text-[#0077b6] file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100"
+                />
+              </div>
 
-        {/* Details Field */}
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Details (Mini blog or description)
-          </label>
-          <textarea
-            name="details"
-            value={details}
-            onChange={(e) => setDetails(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            placeholder="Enter podcast details"
-            rows="4"
-          ></textarea>
-        </div>
+            
 
-        {/* Category Field */}
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Category
-          </label>
-          <input
-            type="text"
-            name="category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            placeholder="Enter podcast category"
-            required
-          />
+              {/* Category Field */}
+              <div className="mb-4 col-span-2">
+                <label className="block text-[#03045e] text-sm font-bold mb-2">
+                  Category
+                </label>
+                <input
+                  type="text"
+                  name="category"
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-[#0077b6] leading-tight focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  placeholder="Enter podcast category"
+                  required
+                />
+              </div>
+              </div>
+              {/* Submit Button */}
+              <div className="flex items-center justify-center">
+                <input
+                  type="submit"
+                  value="Upload Podcast"
+                  className="bg-[#0077b6] hover:bg-[#03045e] w-full px-6 py-3 text-white font-bold  rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                />
+              </div>
+            </form>
+          </div>
         </div>
-
-        {/* Submit Button */}
-        <div className="flex items-center justify-center">
-          <input
-            type="submit"
-            value="Upload Podcast"
-            className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          />
-        </div>
-      </form>
+      </main>
     </div>
   );
 };
