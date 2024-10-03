35968f221508fca79ebf291cee777567dcfabafe
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-10-03 23:44:37 +0600

    added podcast details page, fixed other errors

--- src/app/podcast/[id]/page.jsx
+++ src/app/podcast/[id]/page.jsx
@@ -1,31 +1,63 @@
 import Image from "next/image";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
+import { FaPlay } from "react-icons/fa6";
+import { notFound } from "next/navigation";
+import PodcastDetail from "@/Components/Directory/PodcastDetail";
 
-const page = async ({ params }) => {
+const page = async () => {
 
-    const { id } = params;
+    // const { id } = params;
+    // FOR DYNAMIC ROUTING FROM DB
         // Fetch the podcast details from the deployed API using the ID
-        const response = await fetch(`https://auraloom-backend.vercel.app/podcasts/${id}`, {
-            method: 'GET', // Using GET method to fetch data
-            headers: {
-            'Content-Type': 'application/json',
-            },
-        });
-
+        // const response = await fetch(`http://localhost:5000/podcasts/${id}`, {
+        //     method: 'GET', // Using GET method to fetch data
+        //     headers: {
+        //     'Content-Type': 'application/json',
+        //     },
+        // });
+        
         // Check if the response is okay (status code 200)
-        if (!response.ok) {
-            return <div>Error fetching podcast details.</div>; // Handle error case
-        }
+        // if (!response.ok) {
+        //     return <div>Error fetching podcast details.</div>; // Handle error case
+        // }
 
-        const podcast = await response.json();
+        // const podcast = await response.json();
+        // console.log(podcast);
 
-        if (!podcast) return <div>Loading...</div>;
+        // if (!podcast) return <div>Loading...</div>;
+
 
     return (
         <div>
-            <Image></Image>
-            <h1>{podcast.title}</h1>
-            <p>{podcast.description}</p>
+            <PodcastDetail></PodcastDetail>
         </div>
+        // <div>
+        //     <div className="card bg-[#d4eff4] lg:w-1/2 shadow-xl pt-3 mx-auto mt-10">
+        //         <figure className="w-2/3 rounded-lg mx-auto">
+        //         <Image
+        //             src={podcast.imgUrl}
+        //             layout="responsive"   // Make the image responsive
+        //             width={50}           // Use a relative width, will scale according to container
+        //             height={50}          // Maintain aspect ratio based on the original image dimensions
+        //             className="w-full object-cover"  // object-cover ensures the image is not stretched
+        //             alt="Podcast cover"
+        //         />
+                    
+        //         </figure>
+        //         <div className="card-body">
+        //             <h2 className="card-title flex font-bold lg:text-2xl md:text-xl text-lg">
+        //             {podcast.title}
+        //             {/* <div className="badge p-3 bg-[#34D1F1] justify-items-end">{podcast.played} times</div> */}
+        //             </h2>
+        //             <p className="lg:text-xl font-semibold md:text-lg text-base">{podcast.creator}</p>
+        //             <div className="card-actions justify-end">
+        //             <div className="badge badge-outline p-3  lg:text-lg md:text-base text-sm"><AiOutlineLike  className="text-base mr-1"/> {podcast.likes.length}</div>
+        //             <div className="badge badge-outline p-3  lg:text-lg md:text-base text-sm"><AiOutlineDislike  className="text-base mr-1"/> {podcast.dislikes.length}</div>
+        //             </div>
+        //         </div>
+        //         </div>
+        // </div>
     );
 };
 
