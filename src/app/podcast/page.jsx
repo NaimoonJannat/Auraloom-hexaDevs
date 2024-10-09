e2344ccb44f3ed0db0908173d721607a7dba2a55
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-10-09 21:27:00 +0600

    updated dynamic routes to podcast details page and fixed directory page UI

---
+++ src/app/podcast/page.jsx
@@ -0,0 +1,12 @@
+import PodcastDirectory from "@/Components/Directory/PodcastDirectory";
+
+
+const page = () => {
+    return (
+        <div>
+           <PodcastDirectory></PodcastDirectory>
+        </div>
+    );
+};
+
+export default page;
