9dffc13bda0e95a9c1a319806c235191cea8eb18
Idba1 <moniraislamself@gmail.com>
2024-10-09 16:54:40 +0600

    checking

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -47,6 +47,7 @@ const MainContent = () => {
                             <Link href={'/create-playlist'} className="font-bold text-xl mb-2 hover:bg-sky-700 text-sky-700 hover:text-white px-6 py-3 hover:rounded-md w-full">My Play List</Link>
                         </div>
                     </div>
+                    {/* comment */}
 
                     <div className="min-h-full w-full p-6 bg-white shadow-lg rounded-lg">
                         <h1 className="text-3xl font-bold mb-4 text-blue-950">Your Playlists</h1>
