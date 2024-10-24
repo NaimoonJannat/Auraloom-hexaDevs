57f1a044fe4093426931d67835ca46462bbb44ff
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-10-25 02:01:46 +0600

    Fixed UI for loader in podcast directory

--- src/Components/Directory/PodcastDirectory.jsx
+++ src/Components/Directory/PodcastDirectory.jsx
@@ -129,7 +129,22 @@ const PodcastDirectory = () => {
     // Loading state
     if (isLoading) {
         return (
-            <div className="flex justify-center items-center lg:mt-20">
+            <div className="min-h-screen ">
+            
+            <div className="lg:flex justify-center items-center lg:mt-80 mx-auto hidden ">
+                <CirclesWithBar
+                    height="120"
+                    width="120"
+                    color="#4F46E5"
+                    outerCircleColor="#4F46E5"
+                    innerCircleColor="#4F46E5"
+                    barColor="#4F46E5"
+                    ariaLabel="circles-with-bar-loading"
+                    visible={true}
+                    
+                    />
+            </div>
+            <div className="md:flex justify-center items-center md:mt-72 mx-auto hidden lg:hidden">
                 <CirclesWithBar
                     height="100"
                     width="100"
@@ -139,9 +154,24 @@ const PodcastDirectory = () => {
                     barColor="#4F46E5"
                     ariaLabel="circles-with-bar-loading"
                     visible={true}
-                />
+                    
+                    />
+            </div>            
+            <div className="md:hidden flex justify-center items-center mt-40 mx-auto ">
+                <CirclesWithBar
+                    height="80"
+                    width="80"
+                    color="#4F46E5"
+                    outerCircleColor="#4F46E5"
+                    innerCircleColor="#4F46E5"
+                    barColor="#4F46E5"
+                    ariaLabel="circles-with-bar-loading"
+                    visible={true}
+                    
+                    />
             </div>
-        );
+        </div>
+        )
     }
 
     // No podcasts available
@@ -150,7 +180,7 @@ const PodcastDirectory = () => {
     }
 
     return (
-        <div>
+        <div className="mb-20">
             <SectionTitle title={"Podcast Directory"} />
             {/* Search Bar */}
             <form className="input input-bordered flex w-fit mx-auto items-center gap-2" onSubmit={handleSearchSubmit}>
