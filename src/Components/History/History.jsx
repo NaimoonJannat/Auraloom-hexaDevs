2c46f31d9ac9897bfba5181d9b050a17c47c09e2
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-10-25 02:06:34 +0600

    history page horizontal line correction

--- src/Components/History/History.jsx
+++ src/Components/History/History.jsx
@@ -10,7 +10,7 @@ const History = () => {
         <div>
             <span className="relative flex justify-center my-10 font-bold">
                 <div
-                    className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75 scale-90"
+                    className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75 scale-90 mt-6"
                 ></div>
 
                 <span className="relative z-10  px-6 text-2xl text-[#0077b6]">History</span>
