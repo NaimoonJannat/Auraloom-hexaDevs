fae453c2ab6b793aab05e3c26d2ff8f9868928b5
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-10-31 17:08:35 +0600

    playlist dynamic data fetched

--- src/app/create-playlist/[id]/page.jsx
+++ src/app/create-playlist/[id]/page.jsx
@@ -7,42 +7,10 @@ import AddToPlaylist from '@/Components/AddToPlaylist/AddToPlaylist';
 import { CirclesWithBar } from 'react-loader-spinner';
 
 const PlaylistDetail = ({ params }) => {
-    const { id } = params; // Access the playlist ID directly from params
-    // const [playlist, setPlaylist] = useState(null);
-
-    // useEffect(() => {
-    //     const fetchPlaylist = async () => {
-    //         if (id) {
-    //             try {
-    //                 const response = await axios.get(`https://auraloom-backend.vercel.app/playlists/${id}`);
-    //                 setPlaylist(response.data);
-    //             } catch (error) {
-    //                 console.error('Error fetching playlist:', error);
-    //             }
-    //         }
-    //     };
-    //     fetchPlaylist();
-    // }, [id]);
-
-    // if (!playlist) {
-    //     return (
-    //         <div className="flex justify-center items-center h-screen">
-    //             <CirclesWithBar height={80} width={80} color="#0077b6" />
-    //         </div>
-    //     );
-    // }
+    const { id } = params;
 
     return (
         <div>
-            {/* <h1>{playlist.name}</h1>
-            <Image
-                src={playlist.image || '/default-image.jpg'} // Fallback image path
-                alt={playlist.name || 'Playlist Image'}
-                width={500}
-                height={500}
-                className="h-48 w-full object-cover transition-transform duration-300 group-hover:scale-110"
-            /> */}
-            {/* <p>Created by: {playlist.userName}</p> */}
             <AddToPlaylist id={id} />
         </div>
     );
